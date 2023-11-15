8a7e03cf7282f72868d098564af7cf0836028e3a
Clinton <clixdev07@gmail.com>
2023-11-15 13:13:46 -0500

    .
    .

--- src/main/java/com/clix/server/controller/ProductController.java
+++ src/main/java/com/clix/server/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.clix.server.controller;
 import com.clix.server.model.Product;
 import com.clix.server.repository.ProductoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -15,21 +17,30 @@ public class ProductController {
     @Autowired
     private ProductoRepo productoRepo;
 
-    @RequestMapping
-    public ResponseEntity<List<Product>> getProducts(){
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts() {
         return ResponseEntity.ok((List<Product>) productoRepo.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Optional<Product> productOpt = productoRepo.findById(id);
+        if (productOpt.isPresent()) {
+            return ResponseEntity.ok(productOpt.get());
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productoRepo.save(product));
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productoRepo.deleteById(id);
         return ResponseEntity.ok(null);
     }
-
-
 
 }
